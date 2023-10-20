2e6734b8d94d0d21367bd4e7a0d7db6b727d593a
Katherine Cisneros <19929656+cisnerosk@users.noreply.github.com>
2023-10-20 14:56:01 -0400

    Create Post & User models * Create validations for models

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 5, maximum: 30 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 30}
+  validates :log_in, presence: true
+  validates :email, presence: true
+
+  has_many :posts
+end
